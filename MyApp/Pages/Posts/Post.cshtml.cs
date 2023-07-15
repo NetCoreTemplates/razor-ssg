9da1dfdb964125713b7363f7dca2f14f9dbbbf86
Demis Bellot <demis.bellot@gmail.com>
2023-07-15 09:50:15 +0800

    Update Post with Meta headers

--- MyApp/Pages/Posts/Post.cshtml.cs
+++ MyApp/Pages/Posts/Post.cshtml.cs
@@ -5,47 +5,6 @@ namespace MyApp.Pages.Posts;
 
 public class PostModel : PageModel
 {
-    public MarkdownBlog Blog { get; }
-    public PostModel(MarkdownBlog blog) => Blog = blog;
-
     [FromRoute]
     public string? Slug { get; set; }
-    public string? Layout { get; set; }
-    public string? Title { get; set; }
-    public string? Summary { get; set; }
-    public string? Author { get; set; }
-    public string? Splash { get; set; }
-    public string? AuthorProfileUrl { get; set; }
-    public List<string> Tags { get; set; } = new();
-    public DateTime? Date { get; set; }
-    public int? WordCount { get; set; }
-    public string? HtmlContent { get; set; }
-
-    public void OnGet()
-    {
-        if (string.IsNullOrEmpty(Title) && Slug != null)
-        {
-            var doc = Blog.FindPostBySlug(Slug);
-            if (doc != null)
-            {
-                Populate(doc);
-            }
-        }
-    }
-
-    public PostModel Populate(MarkdownFileInfo doc)
-    {
-        Layout = doc.Layout;
-        Title = doc.Title;
-        Summary = doc.Summary;
-        Slug = doc.Slug;
-        Tags = doc.Tags;
-        Author = doc.Author;
-        AuthorProfileUrl = Blog.GetAuthorProfileUrl(doc.Author);
-        Splash = doc.Image ?? Blog.FallbackSplashUrl;
-        Date = doc.Date;
-        HtmlContent = doc.Preview;
-        WordCount = doc.WordCount;
-        return this;
-    }
 }
---
+++ MyApp/Pages/Shared/MetaHeaders.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Pages.Shared;
+public class MetaHeaders : PageModel
+{
+    public MarkdownFileBase Doc { get; set; }
+    public MetaHeaders(MarkdownFileBase doc) => Doc = doc;
+
+    public string? CanonicalUrl { get; set; }
+    public AuthorInfo? Author { get; set; }
+    public string? Image { get; set; }
+}
