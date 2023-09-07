ba6f8b217fae42d7feb3ec858f058b3f7c50e0b2
GitHub Action <action@github.com>
2023-09-07 19:43:39 +0000

    deploy: 84c69665536f7ed060e4f5e07f8cb05f07bae4b7

--- mjs/app.mjs
+++ mjs/app.mjs
@@ -91,11 +91,6 @@ export function mount(sel, component, props) {
     app.config.compilerOptions.isCustomElement = tag => CustomElements.includes(tag)
     app.mount(el)
     Apps.push(app)
-
-    const { loadMetadata } = useMetadata()
-    loadMetadata({
-        olderThan: 24 * 60 * 60 * 1000, //1day
-    })
     return app
 }
 
@@ -120,6 +115,11 @@ export function mountAll() {
 export function init(exports) {
     if (AppData.init) return
     client = JsonApiClient.create('https://blazor-gallery-api.jamstacks.net')
+    const { loadMetadata } = useMetadata()
+    loadMetadata({
+        olderThan: 24 * 60 * 60 * 1000, //1day
+        resolvePath: `https://blazor-gallery-api.jamstacks.net/metadata/app.json`
+    })
     AppData = reactive(AppData)
     AppData.init = true
     mountAll()
