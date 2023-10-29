02e5d54d194a14e2752a5f9f36cf79c798452b4e
GitHub Action <action@github.com>
2023-10-29 13:17:39 +0000

    deploy: 0564fd1c47e260de5a684e0133d0087f82b8c1f2

--- mjs/components/GettingStarted.mjs
+++ mjs/components/GettingStarted.mjs
@@ -1,4 +1,4 @@
-import { ref } from "vue"
+import { ref, computed } from "vue"
 import ShellCommand from "./ShellCommand.mjs"
 
 export default {
@@ -12,6 +12,26 @@ export default {
       <input type="text" v-model="project" autocomplete="off" spellcheck="false" @keydown="validateSafeName"
              class="mb-8 sm:text-lg rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 dark:bg-gray-800"/>
 
+        <section class="w-full flex justify-center text-center">
+           <div class="mb-2">
+              <div class="flex justify-center text-center">
+                 <a class="archive-url hover:no-underline netcoretemplates_empty" :href="zipUrl('NetCoreTemplates/razor-ssg')">
+                    <div class="bg-white dark:bg-gray-800 px-4 py-4 mr-4 mb-4 rounded-lg shadow-lg text-center items-center justify-center hover:shadow-2xl dark:border-2 dark:border-pink-600 dark:hover:border-blue-600 dark:border-2 dark:border-pink-600 dark:hover:border-blue-600" style="min-width:150px">
+                       <div class="text-center font-extrabold flex items-center justify-center mb-2">
+                          <div class="text-4xl text-blue-400 my-3">
+                             <svg class="w-12 h-12 text-indigo-600" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"><path fill="currentColor" d="M23.844 27.692a16.332 16.332 0 0 1-6.645 1.3q-6.364 0-10.013-3.243a11.3 11.3 0 0 1-3.649-8.9a13.716 13.716 0 0 1 3.785-9.898A12.716 12.716 0 0 1 16.9 3.008a11.676 11.676 0 0 1 8.425 3.006a9.994 9.994 0 0 1 3.142 7.533a10.187 10.187 0 0 1-2.318 7.114a7.532 7.532 0 0 1-5.817 2.547a2.613 2.613 0 0 1-1.845-.642a2.323 2.323 0 0 1-.764-1.6a4.9 4.9 0 0 1-4.148 2.243a4.6 4.6 0 0 1-3.507-1.479a5.706 5.706 0 0 1-1.384-4.063a9.913 9.913 0 0 1 2.2-6.357q2.2-2.763 4.8-2.763a5.063 5.063 0 0 1 4.256 1.716l.311-1.338h2.405l-2.081 9.08a10.716 10.716 0 0 0-.352 2.243q0 .972.744.972a4.819 4.819 0 0 0 3.877-2.047a8.93 8.93 0 0 0 1.621-5.681a7.98 7.98 0 0 0-2.675-6.175a9.887 9.887 0 0 0-6.919-2.432a10.6 10.6 0 0 0-8.158 3.467a12.066 12.066 0 0 0-3.2 8.495a9.561 9.561 0 0 0 3.06 7.573q3.06 2.7 8.586 2.7a13.757 13.757 0 0 0 5.675-1.054ZM19.466 12.25a3.977 3.977 0 0 0-3.6-1.716q-1.824 0-3.263 2.23a8.726 8.726 0 0 0-1.439 4.824q0 3.635 2.905 3.635a3.771 3.771 0 0 0 2.651-1.183a6.309 6.309 0 0 0 1.7-3.2Z"></path></svg>
+                          </div>
+                       </div>
+                       <div class="text-xl font-medium text-gray-700">Razor SSG</div>
+                       <div class="flex justify-center h-8"></div>
+                       <span class="archive-name px-4 pb-2 text-blue-600 dark:text-indigo-400">{{ projectZip }}</span>
+                       <div class="count mt-1 text-gray-400 text-sm"></div>
+                    </div>
+                 </a>
+              </div>
+           </div>
+        </section>
+
       <ShellCommand class="mb-2">dotnet tool install -g x</ShellCommand>
       <ShellCommand class="mb-2">x new {{template}} {{project}}</ShellCommand>
 
@@ -21,16 +41,25 @@ export default {
       <h4 class="py-6 text-center text-xl">Run .NET Project (New Terminal)</h4>
       <ShellCommand class="mb-2">dotnet watch</ShellCommand>
 
-      <div class="mt-8 text-center">
-        <div class="flex justify-center gap-x-2">
-          <PrimaryButton color="purple" href="./posts/rider">rider</PrimaryButton>
-          <PrimaryButton color="purple" href="./posts/vs">visualstudio</PrimaryButton>
-        </div>
-      </div>        
     </div>`,
     props: { template:String },
     setup(props) {
         const project = ref('ProjectName')
+
+        const projectZip = computed(() => (project.value || 'MyApp') + '.zip')
+
+        /** @param {string} template */
+        const zipUrl = (template) =>
+            `https://account.servicestack.net/archive/${template}?Name=${project.value || 'MyApp'}`
+
+        /** @param {KeyboardEvent} e */
+        const isAlphaNumeric = (e) => {
+            const c = e.charCode;
+            if (c >= 65 && c <= 90 || c >= 97 && c <= 122 || c >= 48 && c <= 57 || c === 95) //A-Za-z0-9_
+                return;
+            e.preventDefault()
+        }
+
         /** @param path {string}
           * @returns {string} */
         const resolvePath = (path) => navigator.userAgent.indexOf("Win") >= 0 ? path.replace(/\//g,'\\') : path
@@ -44,6 +73,6 @@ export default {
                 return false
             }
         }
-        return { project, uiPath, apiPath, validateSafeName }
+        return { project, projectZip, zipUrl, isAlphaNumeric, uiPath, apiPath, validateSafeName }
     }
 }
--- mjs/components/ShellCommand.mjs
+++ mjs/components/ShellCommand.mjs
@@ -6,18 +6,28 @@ export default {
     <div class="flex ml-2 w-full justify-between cursor-pointer" @click="copy">
       <div>
           <span>$ </span>
-          <label class="">
+          <label class="cursor-pointer">
             <slot>{{text}}</slot>
           </label>
       </div>
       <small class="text-xs text-gray-400 px-3 -mt-1">sh</small>
     </div>
-    <div v-if="successText" class="-mr-24 right-0 absolute text-md text-gray-200 bg-green-700 px-1 rounded">
-      <div class="flex pr-1">
-        <svg class="w-5 h-5 mt-0.5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="m9.55 18l-5.7-5.7l1.425-1.425L9.55 15.15l9.175-9.175L20.15 7.4Z"/></svg>
-        {{ successText }}
-      </div>
+
+    <div v-if="successText" class="absolute right-0 -mr-28 -mt-3 rounded-md bg-green-50 p-3">
+        <div class="flex">
+            <div class="flex-shrink-0">
+                <svg class="h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                </svg>
+            </div>
+            <div class="ml-3">
+                <p class="text-sm font-medium text-green-800">
+                    {{ successText }}
+                </p>
+            </div>
+        </div>
     </div>
+
   </div>`,
     props:['text'],
     setup(props) {
@@ -26,7 +36,7 @@ export default {
         function copy(e) {
             let $el = document.createElement("input")
             let $lbl = e.target.parentElement.querySelector('label')
-                $el.setAttribute("value", $lbl.innerText)
+            $el.setAttribute("value", $lbl.innerText)
             document.body.appendChild($el)
             $el.select()
             document.execCommand("copy")
