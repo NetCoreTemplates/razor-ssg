6f7a8a960c537e2267d66c053c80e3125a356f83
Demis Bellot <demis.bellot@gmail.com>
2024-09-19 21:52:26 +0800

    update tailwind.config.js

--- MyApp/tailwind.config.js
+++ MyApp/tailwind.config.js
@@ -1,5 +1,31 @@
+const defaultTheme = {
+  fontFamily: {
+    sans: [
+      'ui-sans-serif',
+      'system-ui',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+      '"Noto Color Emoji"'
+    ],
+  }
+}
+
 module.exports = {
-  content: ["./**/*.{html,js,mjs,md,cshtml,razor,cs}","./Pages/**/*.{cshtml,razor}"],
+  content: ["./**/*.{html,js,mjs,md,cshtml,razor}","./Pages/**/*.{cshtml,razor}"],
   darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Satoshi', ...defaultTheme.fontFamily.sans],
+      },
+      spacing: {
+        18: '4.5rem',
+        112: '28rem',
+        120: '30rem',
+      },
+    },
+  },
   plugins: [],
 }
