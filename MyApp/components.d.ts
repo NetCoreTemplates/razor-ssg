deeb6f258387413d23a3c69415986c6d71de734a
Demis Bellot <demis.bellot@gmail.com>
2025-06-27 21:05:24 +0800

    update vue usage

---
+++ MyApp/components.d.ts
@@ -0,0 +1,71 @@
+/* eslint-disable */
+// @ts-nocheck
+// biome-ignore lint: disable
+export {}
+
+/* prettier-ignore */
+declare module '@vue/runtime-core' {
+    export interface GlobalComponents {
+        Alert: typeof import('./wwwroot/lib/typings/@servicestack/vue')['Alert'];
+        AlertSuccess: typeof import('./wwwroot/lib/typings/@servicestack/vue')['AlertSuccess'];
+        ErrorSummary: typeof import('./wwwroot/lib/typings/@servicestack/vue')['ErrorSummary'];
+        InputDescription: typeof import('./wwwroot/lib/typings/@servicestack/vue')['InputDescription'];
+        Icon: typeof import('./wwwroot/lib/typings/@servicestack/vue')['Icon'];
+        Loading: typeof import('./wwwroot/lib/typings/@servicestack/vue')['Loading'];
+        
+        OutlineButton: typeof import('./wwwroot/lib/typings/@servicestack/vue')['OutlineButton'];
+        PrimaryButton: typeof import('./wwwroot/lib/typings/@servicestack/vue')['PrimaryButton'];
+        SecondaryButton: typeof import('./wwwroot/lib/typings/@servicestack/vue')['SecondaryButton'];
+        TextLink: typeof import('./wwwroot/lib/typings/@servicestack/vue')['TextLink'];
+        
+        Breadcrumbs: typeof import('./wwwroot/lib/typings/@servicestack/vue')['Breadcrumbs'];
+        Breadcrumb: typeof import('./wwwroot/lib/typings/@servicestack/vue')['Breadcrumb'];
+        NavList: typeof import('./wwwroot/lib/typings/@servicestack/vue')['NavList'];
+        NavListItem: typeof import('./wwwroot/lib/typings/@servicestack/vue')['NavListItem'];
+        
+        AutoQueryGrid: typeof import('./wwwroot/lib/typings/@servicestack/vue')['AutoQueryGrid'];
+        SettingsIcons: typeof import('./wwwroot/lib/typings/@servicestack/vue')['SettingsIcons'];
+        FilterViews: typeof import('./wwwroot/lib/typings/@servicestack/vue')['FilterViews'];
+        FilterColumn: typeof import('./wwwroot/lib/typings/@servicestack/vue')['FilterColumn'];
+        QueryPrefs: typeof import('./wwwroot/lib/typings/@servicestack/vue')['QueryPrefs'];
+        EnsureAccess: typeof import('./wwwroot/lib/typings/@servicestack/vue')['EnsureAccess'];
+        EnsureAccessDialog: typeof import('./wwwroot/lib/typings/@servicestack/vue')['EnsureAccessDialog'];
+        
+        TextInput: typeof import('./wwwroot/lib/typings/@servicestack/vue')['TextInput'];
+        TextareaInput: typeof import('./wwwroot/lib/typings/@servicestack/vue')['TextareaInput'];
+        SelectInput: typeof import('./wwwroot/lib/typings/@servicestack/vue')['SelectInput'];
+        CheckboxInput: typeof import('./wwwroot/lib/typings/@servicestack/vue')['CheckboxInput'];
+        TagInput: typeof import('./wwwroot/lib/typings/@servicestack/vue')['TagInput'];
+        FileInput: typeof import('./wwwroot/lib/typings/@servicestack/vue')['FileInput'];
+        Autocomplete: typeof import('./wwwroot/lib/typings/@servicestack/vue')['Autocomplete'];
+        Combobox: typeof import('./wwwroot/lib/typings/@servicestack/vue')['Combobox'];
+        DynamicInput: typeof import('./wwwroot/lib/typings/@servicestack/vue')['DynamicInput'];
+        LookupInput: typeof import('./wwwroot/lib/typings/@servicestack/vue')['LookupInput'];
+        
+        AutoFormFields: typeof import('./wwwroot/lib/typings/@servicestack/vue')['AutoFormFields'];
+        AutoForm: typeof import('./wwwroot/lib/typings/@servicestack/vue')['AutoForm'];
+        AutoCreateForm: typeof import('./wwwroot/lib/typings/@servicestack/vue')['AutoCreateForm'];
+        AutoEditForm: typeof import('./wwwroot/lib/typings/@servicestack/vue')['AutoEditForm'];
+        AutoViewForm: typeof import('./wwwroot/lib/typings/@servicestack/vue')['AutoViewForm'];
+        ConfirmDelete: typeof import('./wwwroot/lib/typings/@servicestack/vue')['ConfirmDelete'];
+        FormLoading: typeof import('./wwwroot/lib/typings/@servicestack/vue')['FormLoading'];
+        
+        DataGrid: typeof import('./wwwroot/lib/typings/@servicestack/vue')['DataGrid'];
+        CellFormat: typeof import('./wwwroot/lib/typings/@servicestack/vue')['CellFormat'];
+        PreviewFormat: typeof import('./wwwroot/lib/typings/@servicestack/vue')['PreviewFormat'];
+        HtmlFormat: typeof import('./wwwroot/lib/typings/@servicestack/vue')['HtmlFormat'];
+        MarkupFormat: typeof import('./wwwroot/lib/typings/@servicestack/vue')['MarkupFormat'];
+        MarkupModel: typeof import('./wwwroot/lib/typings/@servicestack/vue')['MarkupModel'];
+        
+        CloseButton: typeof import('./wwwroot/lib/typings/@servicestack/vue')['CloseButton'];
+        SlideOver: typeof import('./wwwroot/lib/typings/@servicestack/vue')['SlideOver'];
+        ModalDialog: typeof import('./wwwroot/lib/typings/@servicestack/vue')['ModalDialog'];
+        ModalLookup: typeof import('./wwwroot/lib/typings/@servicestack/vue')['ModalLookup'];
+        Tabs: typeof import('./wwwroot/lib/typings/@servicestack/vue')['Tabs'];
+        
+        DarkModeToggle: typeof import('./wwwroot/lib/typings/@servicestack/vue')['DarkModeToggle'];
+        SignIn: typeof import('./wwwroot/lib/typings/@servicestack/vue')['SignIn'];
+        MarkdownInput: typeof import('./wwwroot/lib/typings/@servicestack/vue')['MarkdownInput'];
+        SidebarLayout: typeof import('./wwwroot/lib/typings/@servicestack/vue')['SidebarLayout'];
+    }
+}
